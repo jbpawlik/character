c3b2b261093f50393abe9d70d211928d17a4c289
Ben Pawlik <james.benjamin.pawlik@gmail.com>
2021-06-03 15:21:30 -0700

    create form in index.html
    Co-authored-by: Brad Prettyman <brad.prettyman5@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function(event) {
+  event.preventDefault();
+  
+  const age = $('input:radio[name=age]').val()
+
+  if ('input#minor') { 
+
+
+  
+  } else if ('input#adult') 
+
+   if ('input#senior')
+  }
+
+
+  $('#hero').text();
+  $('#whichsuperhero').show()
+});
