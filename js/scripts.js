65c2685b14da83a67dafa97f7da2102880f0acd2
Ben Pawlik <james.benjamin.pawlik@gmail.com>
2021-06-03 16:55:55 -0700

    add radio output
    Co-authored-by: Brad Prettyman <brad.prettyman5@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,17 @@
-$(document).ready(function(event) {
-  event.preventDefault();
-  
-  const age = $('input:radio[name=age]').val()
+$(document).ready(function() {
+  $('form#superhero').submit(function(event) {
+    event.preventDefault();
 
-  if ('input#minor') { 
+    const age = $('input:radio:checked[name=age]').val()
 
-
-  
-  } else if ('input#adult') 
-
-   if ('input#senior')
-  }
-
-
-  $('#hero').text();
-  $('#whichsuperhero').show()
+    if (age === 'minor') {
+      $('#hero').text('Baby');
+    } else if (age === 'adult') {
+      $('#hero').text('Adult');
+    } else if (age === 'senior') {
+      $('#hero').text('Senior')
+    }
+    $('#whichsuperhero').show()
+  })  
 });
+  
